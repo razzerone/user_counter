3bb043b2ba70d829db9525aeac2efb09c71e5a71
KDSolutionss <kondratovdany@gmail.com>
2022-05-26 20:30:09 +0500

    added testss

--- tests.py
+++ tests.py
@@ -1,8 +1,17 @@
 import datetime
 import sqlite3
 import unittest
+from urllib.parse import urlparse
+
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from SQLite_impl import SQLiteRepository
+from app import app
+from database.UserRepository_SqlAlchemy import UsersRepositoryImpl
+from database.VisitsRepository_SqlAlchemy import VisitsRepositoryImpl
+from database.tables import Base, engine
 from smart_repo import SmartRepo
 from user_counter import UserCounter
 
@@ -74,8 +83,71 @@ class SqlTes(unittest.TestCase):
 
 
 class UserCounterTest(unittest.TestCase):
-    def testGetIpCountry(self):
-        self.assertEqual('US', UserCounter.get_ip_country('8.8.8.8'))
+    def test_main(self):
+        response = app.test_client().get('/')
+
+        self.assertIsInstance(response.data, bytes)
+
+    def test_reacheable(self):
+        response = app.test_client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reacheable(self):
+        response = app.test_client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_suc_login_request(self):
+        resp = app.test_client().post('/login', data=dict(username='111', password='111'), follow_redirects=True)
+        self.assertIn('http://localhost/auth', str(resp.request))
+
+    def test_bad_login_request(self):
+        resp = app.test_client().post('/login', data=dict(username='123', password='123'), follow_redirects=True)
+
+        self.assertIn('http://localhost/login', str(resp.request))
+
+    def test_good_validate(self):
+        resp = app.test_client().post('/validate_reg', data=dict(username='111', password='111'), follow_redirects=True)
+        self.assertIn('User already exists', str(resp.data))
+
+    def test_add_counter(self):
+        resp1 = app.test_client().get('/count')
+        app.test_client().post('/login', data=dict(username='111', password='111'), follow_redirects=True)
+        resp2 = app.test_client().get('/count')
+
+        self.assertNotEqual(resp1.data, resp2.data)
+
+    def test_idempotency_counter(self):
+        resp1 = app.test_client().get('/count')
+        resp2 = app.test_client().get('/count')
+
+        self.assertEqual(resp1.data, resp2.data)
+
+    def test_last(self):
+        resp1 = app.test_client().get('/last')
+        app.test_client().post('/login', data=dict(username='111', password='111'), follow_redirects=True)
+        resp2 = app.test_client().get('/last')
+        self.assertNotEqual(resp1.data, resp2.data)
+
+    def test_last_idempotency(self):
+        resp1 = app.test_client().get('/last')
+        resp2 = app.test_client().get('/last')
+        self.assertEqual(resp1.data, resp2.data)
+
+    def test_first_idempotency(self):
+        resp1 = app.test_client().get('/first')
+        resp2 = app.test_client().get('/first')
+        self.assertEqual(resp1.data, resp2.data)
+
+    def test_all_data_onAdd(self):
+        resp1 = app.test_client().get('/all')
+        app.test_client().post('/login', data=dict(username='111', password='111'), follow_redirects=True)
+        resp2 = app.test_client().get('/all')
+        self.assertNotEqual(resp1.data, resp2.data)
+
+    def test_all_data_idempotency(self):
+        resp1 = app.test_client().get('/all')
+        resp2 = app.test_client().get('/all')
+        self.assertEqual(resp1.data, resp2.data)
 
 
 if __name__ == '__main__':
